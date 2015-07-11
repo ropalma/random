9b73272e688d955423adedc81439f5fa185e4eca
ropalma <ropalma@gmail.com>
2015-07-11 13:14:25 -0300

    version 2

--- random.js
+++ random.js
@@ -1,5 +1,7 @@
 var numero = Math.random();
-if (numero <=0.5){console.log('\n' + numero + ' Menor que 0,5  \n');
+var str = ' Mayor que 0,5';
+if (numero <=0.5){
+	str = ' Menor que 0,5';
 }
-else{console.log('\n' + numero + ' Mayor que 0,5  \n');
-}
+console.log('\n' + numero + str);
+
